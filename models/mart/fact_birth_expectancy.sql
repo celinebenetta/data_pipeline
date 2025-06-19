4112a517bdeed67a13b0884c83165b1778e3a26a
celinebenetta <celinebenetta@gmail.com>
2025-06-19 17:26:15 +0800

    create test case in dbt

--- models/mart/fact_birth_expectancy.sql
+++ models/mart/fact_birth_expectancy.sql
@@ -1,5 +1,11 @@
 SELECT
+    {{
+        dbt_utils.generate_surrogate_key (
+            ['sc.country_code', 'ifc.year']
+        )
+    }} AS country_year_key,
     sc.*,
+    year,
     ifc.expected_birth_per_year_country
 FROM
     {{ ref('stg_country_names_area') }} sc
---
+++ tests/expected_birth.sql
@@ -0,0 +1,6 @@
+SELECT
+    *
+FROM
+    {{ ref('fact_birth_expectancy') }}
+WHERE
+    expected_birth_per_year_country < 0
